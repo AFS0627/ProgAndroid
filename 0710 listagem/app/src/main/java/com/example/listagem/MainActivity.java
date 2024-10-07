b2cf8b6edaf4b5f066f3e5da3d1b8fea94c54ef9
Arthur <arthur.fr.sc@gmail.com>
2024-10-07 10:30:22 -0300

    lista

--- 0710 listagem/app/src/main/java/com/example/listagem/MainActivity.java
+++ 0710 listagem/app/src/main/java/com/example/listagem/MainActivity.java
@@ -8,18 +8,17 @@ import android.widget.ListView;
 
 public class MainActivity extends AppCompatActivity {
     String[] nomes = new String[] {"picareta, nome, peco"};
-            ListView listView;
+    ListView listView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        listView=findViewById(R.id.ListView);
-        ArrayAdapter arrayAdapter=new ArrayAdapter(this,
+        listView = findViewById(R.id.listView);
+
+        ArrayAdapter<String> arrayAdapter = new ArrayAdapter<>(this,
                 android.R.layout.simple_list_item_1,
-                android.R.id.text1,
-                nomes
-                );
+                nomes);
         listView.setAdapter(arrayAdapter);
     }
 }
